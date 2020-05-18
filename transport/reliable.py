0473d2a87e407a671eb710281e76d6ee28946374
angelocastro9 <castromatheus060@gmail.com>
2020-05-18 16:00:33 -0300

    Evol

--- transport/reliable.py
+++ transport/reliable.py
@@ -3,9 +3,10 @@ from network.unreliable import UnreliableDataTransfer
 from transport import checksum
 import time
 
-ACK = True
+ACK = False
 Loop = 0
 PacketNumber = 0
+
 
 class ReliableDataTransfer:
 
@@ -17,34 +18,33 @@ class ReliableDataTransfer:
     def send(self, payload):
         global ACK, Loop, PacketNumber
         packet = Packet({'payload': payload})
-        packet.set_field('Pacote', PacketNumber)
+        packet.set_field("Pacote", PacketNumber)
         checksum.calculate_checksum(packet)
-        valid = checksum.validate_checksum(packet)
         self.udt.send(packet)
 
-        while ACK is True:
-            Loop += 1
-            if Loop == 10:
-                print("Resend packet - timeout")
-                if valid and packet is not None:
-                    ACK = False
-                    self.udt.send(packet)
+        while (ACK == False):
+            if (Loop == 10):
+                print("timeout - resend")
+                self.udt.send(packet)
                 Loop = 0
+            Loop += 1
             time.sleep(1)
-        ACK = True
+        ACK = False
         PacketNumber = PacketNumber + 1
         Loop = 0
 
     def receive(self):
         global ACK
-        packet = self.udt.receive(timeout=800)
+        packet = self.udt.receive(timeout=1000)
+        while packet is None:
+            packet = self.udt.receive(timeout=100)
         valid = checksum.validate_checksum(packet)
-        if packet is None:
-            print("Lost packet")
-            return
-        else:
-            ACK = False
-        if valid:
-            return packet.get_field('payload')
-        else:
-            print("invalid checksum")
+        while valid is not True:
+            if valid is False:
+                print("invalid checksum")
+            packet = self.udt.receive(timeout=1000)
+            valid = checksum.validate_checksum(packet)
+
+            if valid:
+                ACK = True
+                return packet.get_field('payload')
